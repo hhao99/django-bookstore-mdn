3007b37ce85194337b6d5f6fffe7450e45b3a3ac
Eric Hao <hhao99@gmail.com>
2023-11-18 19:07:20 -0600

    update the book model and view

--- mybookstore/bookstore/models.py
+++ mybookstore/bookstore/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class BaseModel(models.Model):
+    id = models.AutoField(primary_key=True)
+
+class Book(BaseModel):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=255)
+    summary = models.TextField()
+    pages = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    publish_date = models.DateField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
